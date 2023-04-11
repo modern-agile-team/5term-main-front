21f704ce246b3ee4325edc7501871ec27b1ba354
hyeonjun-L <beeminor60@gmail.com>
2023-04-11 14:09:49 +0900

    publish(feature/hyeonjun/#5): 회원가입 퍼블리싱 수정

--- components/signUp/presentational/block.tsx
+++ components/signUp/presentational/block.tsx
@@ -7,22 +7,26 @@ import {
   SendCertificationBtn,
   CertificationBtn,
   RelativeContainer,
+  FocusServeTitle,
+  AnnotationText,
 } from "./atom";
 import { InputProps } from "interfaces/signUp";
 
-export const InputBlock = ({ title, text }: InputProps) => {
+export const InputBlock = ({ title, validText, placeholder }: InputProps) => {
   return (
     <>
       <InputContainer>
-        <ServeTitle>{title}</ServeTitle>
-        <ValidText>{text}</ValidText>
+        <FocusServeTitle>{title}</FocusServeTitle>
+        <ValidText>{validText}</ValidText>
       </InputContainer>
       <RelativeContainer>
-        <InfoInput />
+        <ServeTitle>{title}</ServeTitle>
+        <AnnotationText>{placeholder}</AnnotationText>
         {title === "핸드폰" && (
           <SendCertificationBtn>인증번호 전송</SendCertificationBtn>
         )}
         {title === "인증번호" && <CertificationBtn>본인인증</CertificationBtn>}
+        <InfoInput />
       </RelativeContainer>
     </>
   );
